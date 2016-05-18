6e8c76dc12e91d799a15e60b67ce0151e744c398
liyocee <collinskivale@gmail.com>
2016-05-18 15:02:54 +0300

    Learned:-
    -Url routing
    -using jinja2 templates
    - url name generation
    - static files path generation
    - lauching dev server in debug mode

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route('/')
-def hello():
-    return "hello world"
+def index():
+    return render_template('home.html', message='Hello world')
+
+
+@app.route('/<person>/<greeting>')
+def greeting(person, greeting):
+    return render_template('greeting.html', message='{}, {}'.format(greeting, person))
 
 
 if __name__ == '__main__':
